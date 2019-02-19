58fed6cc8f6496964e3b64c287a2d1147d8366d4
Leonardo Bianco <leonardo.bianco@outlook.com>
2019-02-19 22:29:14 +0100

    Initial commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos:Todo[];
+
+  constructor() { }
+
+  // E' il metodo inizializzatore del componente, solitamente il costruttore non deve
+  //fare molte cose ma le si fanno fare all'init
+  ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false
+      }
+    ]
+  }
+
+}
